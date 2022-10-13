956da3a759df0b1dff091b4a3c50f69f2d8fbdbb
kknivves <kqualiotto@gmail.com>
2022-10-13 12:49:03 -0700

    Updated Campsites Slice with Redux Toolkit and exports

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+//REPLACE***
+//import counterReducer from '../features/counter/counterSlice'; This was jsut ex. code that comes with Redux template
+import { campsitesReducer } from "../features/campsites/campsitesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: campsitesReducer,
   },
 });
--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,16 +1,30 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { createSlice } from "@reduxjs/toolkit"; // import function from toolkit library
 
+import { CAMPSITES } from "../../app/shared/CAMPSITES";
+
+const initialState = {
+  // setup initialState object that will pass to createSlice
+  campsitesArray: CAMPSITES, //set single property name campsitesArray
+};
+
+const campsitesSlice = createSlice({
+  //set this equal to object that's returned from calling createSlice
+  name: "campsites", //need to pass data to createSlice in the form of an object with two properties
+  initialState, // since key and object name for value are the same, we can just write object once
+});
+
+export const campsitesReducer = campsitesSlice.reducer; //export as function
 
 export const selectAllCampsites = () => {
-    return CAMPSITES;
+  return CAMPSITES;
 };
 
 export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+  return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite) => campsite.featured);
+  return CAMPSITES.find((campsite) => campsite.featured);
 };
 
 // export const selectRandomCampsite = () => {
