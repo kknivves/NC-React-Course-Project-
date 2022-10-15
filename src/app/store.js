100b6de7d5df96089ec99ed1347de895d5756ecf
kknivves <kqualiotto@gmail.com>
2022-10-14 23:14:38 -0700

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { getDefaultNormalizer } from "@testing-library/react";
+import logger from "redux-logger";
 import { campsitesReducer } from "../features/campsites/campsitesSlice";
 import { commentsReducer } from "../features/comments/commentsSlice";
 import { partnersReducer } from "../features/partners/partnersSlice";
@@ -9,6 +11,7 @@ export const store = configureStore({
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionsReducer
-  }
+    promotions: promotionsReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger]),
 });
